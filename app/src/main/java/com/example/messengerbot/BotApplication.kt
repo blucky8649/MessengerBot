cb6320c129a0fc3001ddd884922c5604d8a8eeeb
blucky8649 <blucky4511@gmail.com>
2023-08-24 01:29:39 +0900

    delete unused code

--- app/src/main/java/com/example/messengerbot/BotApplication.kt
+++ app/src/main/java/com/example/messengerbot/BotApplication.kt
@@ -1,8 +1,6 @@
 package com.example.messengerbot
 
 import android.app.Application
-import com.example.messengerbot.service.MessengerBotService
-import com.example.messengerbot.settings.Command
 
 class BotApplication : Application() {
 
